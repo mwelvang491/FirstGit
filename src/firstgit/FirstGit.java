71c02dfc830b9e7a1a89989c8ac4776b4cda766f
mitchell <mitchell@10.30.21.247>
2017-01-26 20:56:10 -0600

    Added "Hello World" to main method.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -10,12 +10,13 @@ package firstgit;
  * @author mitchell
  */
 public class FirstGit {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
